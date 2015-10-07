68b2901435a38e9580d89df5d9df204781150385
ajchan11 <ajchan11@gmail.com>
2015-10-07 09:08:15 -0700

    alex destroys the users controllers

--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -1,117 +1,29 @@
-var express = require('express')
-var router = express.Router()
-var usersController = require('../controllers/usersControllers')
-var User = require('../models/User')
-var jwt = require('jsonwebtoken')
-var passport = require( "passport" )
-var mySpecialSecret = "threeamigos"
+var
+  express = require( "express" ),
+  app = express(),
+  // Parses information from POST
+  bodyParser = require( "body-parser" ),
+  // Used to manipulate POST methods
+  methodOverride = require( "method-override" ),
+  passport = require( "passport" ),
+  usersController = require( "../controllers/usersControllers" );
 
-module.exports = function( passport ) {
+function authenticatedUser( req, res, next ) {
+  // If the user is authenticated, then we continue the execution
+  if ( req.isAuthenticated() ) {
+      return next();
+  }
 
-// INDEX
-// router.route( "/" )
-// 	.get( usersController.index )
-
-// // LOGIN
-// router.route( "/login" )
-// 	// .get( usersController.loginPage )
-// 	.post( usersController.login )
-
-// // PROFILE
-// router.route( "/profile")
-// 	.get( usersController.profilePage )
-
-// //	LOGOUT
-// router.route( "/logout" )
-// 	// .get( passport.authenticate( 'local-login', {
-// 	// 		successRedirect : '/profile',
-// 	// 		failureRedirect : '/login'
-// 	// 	}), usersController.logout )
-
-//	SIGN UP
-router.route( "/signup" )
-	// .get( usersController.signupPage )
-	.post( passport.authenticate( 'local-signup', {
-			successRedirect : 'https://www.google.com',
-			failureRedirect : '/login'
-		}), usersController.create )
-
-	return router
-
+  // Otherwise the request is always redirected to the home page
+  res.redirect( '/' );
 }
-/*
-// set up index/get for api router
-apiRouter.route('/users')
-	.get(usersController.index)
-	// for creating a new user
-	.post(usersController.create)
 
-apiRouter.route('/users/:user_id')
-	// this is the show action 
-	.get(usersController.show)
+app.get("/signup", usersController.getSignup );
+app.post("/signup", usersController.postSignup );
 
+app.get("/login", usersController.getLogin );
+app.post("/login", usersController.postLogin );
 
-apiRouter.route('/authenticate')
-	.post(function(req, res){
-	console.log('trying to create a JWT')
-	// try to find the user in the db
-	User.findOne({
-		username: req.body.username
-	}).select('name username password').exec(function(err, user){
-		if(err) throw err
-		if(!user){
-			res.json({success: false, message: "auth attempt failed"})
-		} else if(user){
-			// check the password
-			var validPassword = user.comparePassword(req.body.password)
-			if(!validPassword){
-				res.json({success: false, message: "auth attempt failed, re-check your username and password"})
-			} else {
-				// password matches, move on
-				var token = jwt.sign({
-					name: user.name,
-					username: user.username
-				}, mySpecialSecret, {
-					expiresInSeconds: 1440
-				})
-				// give the user the token
-				res.json({ success: true, message: "token is successful", token: token})
-			}
-		}
-	})
-})
+app.get("/logout", usersController.getLogout );
 
-apiRouter.use(function(req, res, next){
-	// check mulitple places for the JWT!
-	var token = req.body.token || req.params.token || req.headers['x-access-token']
-
-	// if token is found, use mySpecialSecret to decode.
-	if(token){
-		jwt.verify(token, mySpecialSecret, function(err, decoded){
-			if(err){
-				res.status(403).send({success: false, message: "problem with your decoder ring"})
-			} else {
-				req.decoded = decoded
-				next()
-			}
-		})
-	} else {
-		res.status(403).send({success: false, message: "dude, where's my token"})
-	}
-})
-
-apiRouter.use(function(req,res,next){
-	// Need to authenticate, just send message for now
-	console.log('someone is using our API, we will authticate them here')
-	// if not logged in res.json({message: "please log in"})
-	// else, next ()
-	next()
-})
-
-	// this is the update action
-	.put(usersController.update)
-
-	// this is the destroy function
-	.delete(usersController.destroy)
-
-module.exports = apiRouter*/
+module.exports = app;
--- server.js
+++ server.js
@@ -3,12 +3,14 @@ var app = express();
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var mongoose = require('mongoose');
-var port = process.env.PORT || 3000;
 var passport = require('passport');
-var userRoutes = require('./app/routes/userRoutes')( passport );
 var path = require('path')
 var session = require('express-session');
 var ejsLayouts = require("express-ejs-layouts");
+var routes = require('./app/routes/userRoutes');
+var flash = require( "connect-flash" );
+
+
 
 
 mongoose.connect('localhost:27017/project_3')
@@ -39,15 +41,14 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session()); //persistent login session
 app.use(function (req, res, next){
-    //console.log(req.user)
     global.user = req.user;
     next()
 });
+app.use( flash() ); 
 
-// write a simple get '/' route for the api
-/*apiRouter.get('/', function(req, res){
-    res.render('{message: "welcome to the api"}')
-})*/
+app.get('/', function(req, res){
+    res.render('index')
+})
 
 app.get('/flickr', function(req, res){
     res.render('flickr')
@@ -56,7 +57,7 @@ app.get('/flickr', function(req, res){
 // tell app to use apiRoutes when we go to 
 // localhost:3000/api
 require("./app/config/passport")(passport)
-app.use( "/", userRoutes )
+app.use( routes );
 
 //RUN THE SERVER
 
